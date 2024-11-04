73850c910d0a19eb80823be7c91a91ec4d442aef
sujnkim <blackout3210@naver.com>
2024-11-04 17:07:07 +0900

    test(travel): TravelService 여행 수정, 삭제 로직 테스트코드 추가

--- src/test/java/swyp/swyp6_team7/travel/service/TravelServiceTest.java
+++ src/test/java/swyp/swyp6_team7/travel/service/TravelServiceTest.java
@@ -6,29 +6,25 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.transaction.annotation.Transactional;
 import swyp.swyp6_team7.location.domain.Location;
 import swyp.swyp6_team7.location.domain.LocationType;
 import swyp.swyp6_team7.location.repository.LocationRepository;
-import swyp.swyp6_team7.member.entity.AgeGroup;
-import swyp.swyp6_team7.member.entity.Gender;
-import swyp.swyp6_team7.member.entity.UserStatus;
-import swyp.swyp6_team7.member.entity.Users;
 import swyp.swyp6_team7.tag.service.TravelTagService;
 import swyp.swyp6_team7.travel.domain.GenderType;
 import swyp.swyp6_team7.travel.domain.PeriodType;
 import swyp.swyp6_team7.travel.domain.Travel;
 import swyp.swyp6_team7.travel.domain.TravelStatus;
 import swyp.swyp6_team7.travel.dto.request.TravelCreateRequest;
+import swyp.swyp6_team7.travel.dto.request.TravelUpdateRequest;
 import swyp.swyp6_team7.travel.repository.TravelRepository;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.BDDMockito.given;
 
 @SpringBootTest
@@ -40,28 +36,26 @@ class TravelServiceTest {
     @Autowired
     private TravelRepository travelRepository;
 
+    @Autowired
+    private LocationRepository locationRepository;
+
     @Mock
     private TravelTagService travelTagService;
 
-    @Mock
-    private LocationRepository locationRepository;
 
     @AfterEach
     void tearDown() {
         travelRepository.deleteAllInBatch();
+        locationRepository.deleteAllInBatch();
     }
 
     @DisplayName("create: 여행 콘텐츠를 만들 수 있다")
     @Test
-    @DirtiesContext
-    public void createTravelWithUser() {
+    public void create() {
         // given
-        travelRepository.deleteAllInBatch();
-
-        Users user = createUser();
-        Location travelLocation = new Location(1L, "서울", LocationType.DOMESTIC);
-
+        Location location = locationRepository.save(createLocation("Seoul"));
         LocalDate dueDate = LocalDate.of(2024, 11, 4);
+
         TravelCreateRequest request = TravelCreateRequest.builder()
                 .locationName("Seoul")
                 .title("여행 제목")
@@ -74,13 +68,11 @@ class TravelServiceTest {
                 .completionStatus(true)
                 .build();
 
-        given(locationRepository.findByLocationName(anyString()))
-                .willReturn(Optional.of(travelLocation));
         given(travelTagService.create(any(Travel.class), anyList()))
                 .willReturn(List.of());
 
         // when
-        Travel createdTravel = travelService.create(request, user.getUserNumber());
+        Travel createdTravel = travelService.create(request, 1);
 
         // then
         assertThat(travelRepository.findAll()).hasSize(1);
@@ -100,16 +92,131 @@ class TravelServiceTest {
         assertThat(createdTravel.getDeletedUser()).isNull();
     }
 
-    private Users createUser() {
-        return Users.builder()
-                .userNumber(1)
-                .userEmail("test@naver.com")
-                .userPw("1234")
-                .userName("사용자명")
-                .userGender(Gender.M)
-                .userAgeGroup(AgeGroup.TWENTY)
-                .userRegDate(LocalDateTime.now())
-                .userStatus(UserStatus.ABLE)
+    @DisplayName("update: 여행 콘텐츠를 수정할 수 있다.")
+    @Transactional
+    @Test
+    void update() {
+        // given
+        int hostUserNumber = 1;
+        Location location = locationRepository.save(createLocation("Seoul"));
+        LocalDate dueDate = LocalDate.of(2024, 11, 4);
+        Travel savedTravel = travelRepository.save(createTravel(hostUserNumber, location, dueDate, TravelStatus.IN_PROGRESS));
+
+        TravelUpdateRequest request = TravelUpdateRequest.builder()
+                .locationName("Seoul")
+                .title("여행 제목 수정")
+                .details("여행 내용 수정")
+                .maxPerson(3)
+                .genderType(GenderType.WOMAN_ONLY.toString())
+                .dueDate(LocalDate.of(2024, 11, 5))
+                .periodType(PeriodType.MORE_THAN_MONTH.toString())
+                .tags(List.of())
+                .completionStatus(true)
+                .build();
+
+        given(travelTagService.update(any(Travel.class), anyList()))
+                .willReturn(List.of());
+
+        // when
+        Travel updatedTravel = travelService.update(savedTravel.getNumber(), request, 1);
+
+        // then
+        assertThat(travelRepository.findAll()).hasSize(1);
+        assertThat(updatedTravel.getUserNumber()).isEqualTo(1);
+        assertThat(updatedTravel.getLocationName()).isEqualTo("Seoul");
+        assertThat(updatedTravel.getTitle()).isEqualTo("여행 제목 수정");
+        assertThat(updatedTravel.getDetails()).isEqualTo("여행 내용 수정");
+        assertThat(updatedTravel.getMaxPerson()).isEqualTo(3);
+        assertThat(updatedTravel.getGenderType()).isEqualTo(GenderType.WOMAN_ONLY);
+        assertThat(updatedTravel.getDueDate()).isEqualTo(LocalDate.of(2024, 11, 5));
+        assertThat(updatedTravel.getPeriodType()).isEqualTo(PeriodType.MORE_THAN_MONTH);
+        assertThat(updatedTravel.getStatus()).isEqualTo(TravelStatus.IN_PROGRESS);
+        assertThat(updatedTravel.getTravelTags()).isEmpty();
+    }
+
+    @DisplayName("update: 여행 콘텐츠를 수정할 때, 작성자가 아니라면 예외가 발생한다.")
+    @Test
+    void updateWhenNotHost() {
+        // given
+        int hostUserNumber = 1;
+        int requestUserNumber = 2;
+
+        Location location = locationRepository.save(createLocation("Seoul"));
+        LocalDate dueDate = LocalDate.of(2024, 11, 4);
+        Travel savedTravel = travelRepository.save(createTravel(hostUserNumber, location, dueDate, TravelStatus.IN_PROGRESS));
+
+        TravelUpdateRequest request = TravelUpdateRequest.builder()
+                .tags(List.of())
+                .completionStatus(true)
+                .build();
+
+        // when //then
+        assertThatThrownBy(() -> {
+            travelService.update(savedTravel.getNumber(), request, requestUserNumber);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("여행 수정 권한이 없습니다.");
+    }
+
+    @DisplayName("delete: 여행 콘텐츠를 삭제할 수 있다.")
+    @Test
+    void delete() {
+        // given
+        int hostUserNumber = 1;
+        Location location = locationRepository.save(createLocation("Seoul"));
+        LocalDate dueDate = LocalDate.of(2024, 11, 4);
+        Travel savedTravel = travelRepository.save(createTravel(hostUserNumber, location, dueDate, TravelStatus.IN_PROGRESS));
+
+        // when
+        travelService.delete(savedTravel.getNumber(), hostUserNumber);
+
+        // then
+        List<Travel> travels = travelRepository.findAll();
+        assertThat(travels).hasSize(1)
+                .extracting("number", "status")
+                .contains(
+                        tuple(savedTravel.getNumber(), TravelStatus.DELETED)
+                );
+    }
+
+    @DisplayName("delete: 여행 콘텐츠를 삭제할 때, 작성자가 아니라면 예외가 발생한다.")
+    @Test
+    void deleteWhenNotHost() {
+        // given
+        int hostUserNumber = 1;
+        int requestUserNumber = 2;
+
+        Location location = locationRepository.save(createLocation("Seoul"));
+        LocalDate dueDate = LocalDate.of(2024, 11, 4);
+        Travel savedTravel = travelRepository.save(createTravel(hostUserNumber, location, dueDate, TravelStatus.IN_PROGRESS));
+
+        // when //then
+        assertThatThrownBy(() -> {
+            travelService.delete(savedTravel.getNumber(), requestUserNumber);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("여행 삭제 권한이 없습니다.");
+    }
+
+
+    private Location createLocation(String locationName) {
+        return Location.builder()
+                .locationName(locationName)
+                .locationType(LocationType.DOMESTIC)
+                .build();
+    }
+
+    private Travel createTravel(int hostUserNumber, Location location, LocalDate dueDate, TravelStatus status) {
+        return Travel.builder()
+                .userNumber(hostUserNumber)
+                .location(location)
+                .locationName(location.getLocationName())
+                .title("여행 제목")
+                .details("여행 내용")
+                .viewCount(0)
+                .maxPerson(2)
+                .genderType(GenderType.MIXED)
+                .dueDate(dueDate)
+                .periodType(PeriodType.ONE_WEEK)
+                .status(status)
                 .build();
     }
 
