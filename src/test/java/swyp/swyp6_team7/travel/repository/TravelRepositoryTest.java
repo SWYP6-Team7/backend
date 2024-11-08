360a2c73b8de8b4a44964d35eaff59d5e832d07a
sujnkim <blackout3210@naver.com>
2024-11-08 14:46:09 +0900

    test(travel): TravelRepository 테스트 추가

---
+++ src/test/java/swyp/swyp6_team7/travel/repository/TravelRepositoryTest.java
@@ -0,0 +1,100 @@
+package swyp.swyp6_team7.travel.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import swyp.swyp6_team7.config.DataConfig;
+import swyp.swyp6_team7.location.domain.Location;
+import swyp.swyp6_team7.location.domain.LocationType;
+import swyp.swyp6_team7.location.repository.LocationRepository;
+import swyp.swyp6_team7.travel.domain.GenderType;
+import swyp.swyp6_team7.travel.domain.PeriodType;
+import swyp.swyp6_team7.travel.domain.Travel;
+import swyp.swyp6_team7.travel.domain.TravelStatus;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Import(DataConfig.class)
+@DataJpaTest
+class TravelRepositoryTest {
+
+    @Autowired
+    private TravelRepository travelRepository;
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+
+    @DisplayName("TravelNumber가 주어질 때, 해당 Travel의 viewCount를 1 증가시킨다.")
+    @Test
+    void updateViewCountPlusOneByTravelNumber() {
+        // given
+        int nowViewCount = 5;
+        Travel travel = travelRepository.save(createTravel(nowViewCount, LocalDateTime.now()));
+
+        // when
+        travelRepository.updateViewCountPlusOneByTravelNumber(travel.getNumber());
+
+        // then
+        assertThat(travelRepository.findAll()).hasSize(1)
+                .extracting("viewCount")
+                .contains(6);
+    }
+
+    @DisplayName("TravelNumber가 주어질 때, 해당 Travel의 enrollmentLastViewedAt를 가져올 수 있다.")
+    @Test
+    void getEnrollmentsLastViewedAtByNumber() {
+        // given
+        LocalDateTime viewedAt = LocalDateTime.of(2024, 11, 8, 12, 0);
+        Travel travel = travelRepository.save(createTravel(0, viewedAt));
+
+        // when
+        LocalDateTime result = travelRepository.getEnrollmentsLastViewedAtByNumber(travel.getNumber());
+
+        // then
+        assertThat(result)
+                .isEqualTo(LocalDateTime.of(2024, 11, 8, 12, 0));
+    }
+
+    @DisplayName("TravelNumber와 LocalDateTime이 주어질 때, 해당 Travel의 enrollmentLastViewedAt의 값을 수정한다.")
+    @Test
+    void updateEnrollmentsLastViewedAtByNumber() {
+        // given
+        LocalDateTime viewedAt = LocalDateTime.of(2024, 11, 8, 12, 0);
+        Travel travel = travelRepository.save(createTravel(0, viewedAt));
+
+        LocalDateTime updateViewedAt = LocalDateTime.of(2024, 11, 8, 15, 0);
+
+        // when
+        travelRepository.updateEnrollmentsLastViewedAtByNumber(travel.getNumber(), updateViewedAt);
+
+        // then
+        assertThat(travelRepository.findAll()).hasSize(1)
+                .extracting("enrollmentsLastViewedAt")
+                .contains(LocalDateTime.of(2024, 11, 8, 15, 0));
+    }
+
+
+    private Travel createTravel(int nowViewCount, LocalDateTime enrollmentLastViewdAt) {
+        Location location = locationRepository.save(Location.builder()
+                .locationName("여행지명")
+                .locationType(LocationType.DOMESTIC)
+                .build());
+
+        return Travel.builder()
+                .userNumber(1)
+                .location(location)
+                .viewCount(nowViewCount)
+                .genderType(GenderType.MIXED)
+                .periodType(PeriodType.ONE_WEEK)
+                .status(TravelStatus.IN_PROGRESS)
+                .enrollmentsLastViewedAt(enrollmentLastViewdAt)
+                .build();
+    }
+
+}
+
