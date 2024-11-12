537de7842271b639a58cebd2ab7a73b671ffad1d
sujnkim <blackout3210@naver.com>
2024-11-12 15:11:44 +0900

    feat(exception): validate BindException 전역 예외 처리 메서드 추가

--- src/main/java/swyp/swyp6_team7/global/exception/GlobalExceptionHandler.java
+++ src/main/java/swyp/swyp6_team7/global/exception/GlobalExceptionHandler.java
@@ -2,7 +2,9 @@ package swyp.swyp6_team7.global.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -17,4 +19,12 @@ public class GlobalExceptionHandler {
         // 클라이언트에게 서버 에러 메시지 전송
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 에러: " + ex.getMessage());
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<String> bindException(BindException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+    }
+
 }
