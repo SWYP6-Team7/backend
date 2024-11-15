20e4457984c73a4d518ee04a1777ca0eb267c027
sujnkim <blackout3210@naver.com>
2024-11-15 17:30:09 +0900

    test(enrollment): 여행 신청 목록 조회 테스트 추가

--- src/test/java/swyp/swyp6_team7/travel/controller/TravelEnrollmentControllerTest.java
+++ src/test/java/swyp/swyp6_team7/travel/controller/TravelEnrollmentControllerTest.java
@@ -1,195 +1,75 @@
 package swyp.swyp6_team7.travel.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import swyp.swyp6_team7.enrollment.domain.Enrollment;
 import swyp.swyp6_team7.enrollment.domain.EnrollmentStatus;
-import swyp.swyp6_team7.enrollment.repository.EnrollmentRepository;
-import swyp.swyp6_team7.location.domain.Location;
-import swyp.swyp6_team7.location.domain.LocationType;
-import swyp.swyp6_team7.location.repository.LocationRepository;
+import swyp.swyp6_team7.enrollment.dto.EnrollmentResponse;
+import swyp.swyp6_team7.enrollment.service.EnrollmentService;
 import swyp.swyp6_team7.member.entity.AgeGroup;
-import swyp.swyp6_team7.member.entity.Gender;
-import swyp.swyp6_team7.member.entity.UserStatus;
-import swyp.swyp6_team7.member.entity.Users;
-import swyp.swyp6_team7.member.repository.UserRepository;
-import swyp.swyp6_team7.travel.domain.GenderType;
-import swyp.swyp6_team7.travel.domain.PeriodType;
-import swyp.swyp6_team7.travel.domain.Travel;
-import swyp.swyp6_team7.travel.domain.TravelStatus;
-import swyp.swyp6_team7.travel.repository.TravelRepository;
+import swyp.swyp6_team7.mock.WithMockCustomUser;
+import swyp.swyp6_team7.travel.dto.response.TravelEnrollmentsResponse;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.UUID;
+import java.util.List;
 
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource(properties = {
-        "kakao.client-id=fake-client-id",
-        "kakao.client-secret=fake-client-secret",
-        "kakao.redirect-uri=http://localhost:8080/login/oauth2/code/kakao",
-        "kakao.token-url=https://kauth.kakao.com/oauth/token",
-        "kakao.user-info-url=https://kapi.kakao.com/v2/user/me"
-})
 class TravelEnrollmentControllerTest {
 
     @Autowired
-    protected MockMvc mockMvc;
-    @Autowired
-    protected ObjectMapper objectMapper;
-    @Autowired
-    private WebApplicationContext context;
-    @Autowired
-    private UserDetailsService userDetailsService;
+    private MockMvc mockMvc;
 
     @Autowired
-    TravelRepository travelRepository;
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    EnrollmentRepository enrollmentRepository;
-    @Autowired
-    LocationRepository locationRepository;
+    private ObjectMapper objectMapper;
 
-    Travel travel;
-    Users host;
+    @MockBean
+    private EnrollmentService enrollmentService;
 
-    @BeforeEach
-    void mockMvcSetup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
-                .build();
-    }
 
-    @BeforeEach
-    void setSecurityContext() {
-        userRepository.deleteAll();
-        host = userRepository.save(Users.builder()
-                .userEmail("host@test.com")
-                .userPw("1234")
-                .userName("host")
-                .userGender(Gender.M)
-                .userAgeGroup(AgeGroup.TEEN)
-                .userRegDate(LocalDateTime.now())
-                .userStatus(UserStatus.ABLE)
-                .build()
-        );
-    }
-
-    @BeforeEach
-    void setTravel() {
-        Location travelLocation = Location.builder()
-                .locationName("Seoul"+ UUID.randomUUID().toString())
-                .locationType(LocationType.DOMESTIC)
-                .build();
-        Location savedLocation = locationRepository.save(travelLocation);
-        travel = travelRepository.save(Travel.builder()
-                .title("기본 여행")
-                .userNumber(host.getUserNumber())
-                .maxPerson(3)
-                .genderType(GenderType.NONE)
-                .dueDate(LocalDate.now().plusDays(5))
-                .periodType(PeriodType.NONE)
-                .status(TravelStatus.IN_PROGRESS)
-                        .location(savedLocation)
-                .build()
-        );
-    }
-
-    @AfterEach
-    void tearDown() {
-        enrollmentRepository.deleteAllInBatch();
-        travelRepository.deleteAllInBatch();
-        userRepository.deleteAllInBatch();
-    }
-
-    @DisplayName("findElements: 주최자는 특정 여행에 대한 참가 신청서 목록을 조회할 수 있다")
+    @DisplayName("findEnrollments: 특정 여행에 대한 참가 신청서 목록을 조회할 수 있다")
+    @WithMockCustomUser
     @Test
-    @DirtiesContext
-    public void findElementsWithHost() throws Exception {
+    public void findEnrollments() throws Exception {
         // given
-        String url = "/api/travel/{travelNumber}/enrollments";
-        Users user = userRepository.save(Users.builder()
-                .userEmail("abc@test.com")
-                .userPw("1234")
-                .userName("username")
-                .userGender(Gender.M)
-                .userAgeGroup(AgeGroup.TEEN)
-                .userRegDate(LocalDateTime.now())
-                .userStatus(UserStatus.ABLE)
-                .build());
-        createEnrollment(user);
+        EnrollmentResponse enrollment1 = EnrollmentResponse.builder()
+                .enrollmentNumber(1)
+                .userName("신청자명")
+                .ageGroup(AgeGroup.TEEN)
+                .profileUrl("profile-url")
+                .message("여행 신청")
+                .status(EnrollmentStatus.PENDING)
+                .build();
 
-        var userDetails = userDetailsService.loadUserByUsername(host.getUserEmail());
-        SecurityContext context = SecurityContextHolder.getContext();
-        context.setAuthentication(new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities()));
+        TravelEnrollmentsResponse response = TravelEnrollmentsResponse.from(List.of(enrollment1));
+
+        given(enrollmentService.findEnrollmentsByTravelNumber(anyInt(), anyInt()))
+                .willReturn(response);
 
         // when
-        ResultActions resultActions = mockMvc.perform(get(url, travel.getNumber()));
+        ResultActions resultActions = mockMvc.perform(get("/api/travel/{travelNumber}/enrollments", 1));
 
         // then
         resultActions
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.totalCount").value(1))
-                .andExpect(jsonPath("$.enrollments[0].userName").value(user.getUserName()));
-    }
-
-    @DisplayName("findElements: 주최자가 아니면 특정 여행에 대한 참가 신청서 목록을 조회할 수 없다")
-    @Test
-    public void findElementsWithNotHost() throws Exception {
-        // given
-        String url = "/api/travel/{travelNumber}/enrollments";
-        Users user = userRepository.save(Users.builder()
-                .userEmail("abc@test.com")
-                .userPw("1234")
-                .userName("username")
-                .userGender(Gender.M)
-                .userAgeGroup(AgeGroup.TEEN)
-                .userRegDate(LocalDateTime.now())
-                .userStatus(UserStatus.ABLE)
-                .build());
-        createEnrollment(user);
-
-        var userDetails = userDetailsService.loadUserByUsername(user.getUserEmail());
-        SecurityContext context = SecurityContextHolder.getContext();
-        context.setAuthentication(new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities()));
-
-        // when
-        ResultActions resultActions = mockMvc.perform(get(url, travel.getNumber()));
-
-        // then
-        resultActions
-                .andExpect(status().is5xxServerError())
-                .andExpect(content().string("서버 에러: " + "여행 주최자의 권한이 필요한 작업입니다."));
-    }
-
-
-    private void createEnrollment(Users users) {
-        enrollmentRepository.save(Enrollment.builder()
-                .userNumber(users.getUserNumber())
-                .travelNumber(travel.getNumber())
-                .message("참가 신청합니다.")
-                .status(EnrollmentStatus.PENDING)
-                .build());
+                .andExpect(jsonPath("$.enrollments[0].enrollmentNumber").value(1))
+                .andExpect(jsonPath("$.enrollments[0].userName").value("신청자명"))
+                .andExpect(jsonPath("$.enrollments[0].userAgeGroup").value("10대"))
+                .andExpect(jsonPath("$.enrollments[0].profileUrl").value("profile-url"))
+                .andExpect(jsonPath("$.enrollments[0].message").value("여행 신청"))
+                .andExpect(jsonPath("$.enrollments[0].status").value("대기"));
     }
 
 }
