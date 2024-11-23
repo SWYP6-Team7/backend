a772473de0afafd30a18e183a0751ace8dff64c1
sujnkim <blackout3210@naver.com>
2024-11-23 15:59:00 +0900

    style(image): S3KeyHandler 주석 수정 및 로그 추가

--- src/main/java/swyp/swyp6_team7/image/util/S3KeyHandler.java
+++ src/main/java/swyp/swyp6_team7/image/util/S3KeyHandler.java
@@ -1,66 +1,73 @@
 package swyp.swyp6_team7.image.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import swyp.swyp6_team7.image.s3.FileFolder;
 import swyp.swyp6_team7.image.s3.S3Component;
 
 
+@Slf4j
 @Component
 public class S3KeyHandler {
-    private final String baseFolder;
-    private final S3Component s3Component;
 
+    private final static String S3_REGION = "ap-northeast-2";
+
+    private final S3Component s3Component;
+    private final String baseFolder;
+    private final String bucketName;
 
     @Autowired
     public S3KeyHandler(S3Component s3Component) {
-        //베이스 폴더 가져오기
-        this.baseFolder = s3Component.getBaseFolder();
-        //relatedType이 profile일때 relatedNumber는 userNumber
         this.s3Component = s3Component;
+        this.baseFolder = s3Component.getBaseFolder(); //베이스 폴더 가져오기
+        this.bucketName = s3Component.getBucket();
     }
 
-    // 동적으로 key 생성 메소드 {baseFolder}/{rleatedType}/{id}/{file_name}
+
+    // 동적으로 key 생성 메소드: {baseFolder}/{relatedType}/{id}/{file_name}
+    // relatedType이 profile일때 relatedNumber는 userNumber
     public String generateS3Key(String relatedType, int relatedNumber, String storageName, int order) {
+
         FileFolder folderType = FileFolder.from(relatedType);
 
-        //profile 인 경우
+        // profile인 경우
         if (folderType == FileFolder.PROFILE) {
-                return baseFolder + folderType.name().toLowerCase() + "/" + relatedNumber + "/" + storageName;
+            return baseFolder + folderType.name().toLowerCase() + "/" + relatedNumber + "/" + storageName;
         }
 
-        //커뮤니티 인경우
+        // 커뮤니티인 경우
         else if (folderType == FileFolder.COMMUNITY) {
-                return baseFolder + folderType.name().toLowerCase() + "/" + relatedNumber + "/" + (order > 0 ? order + "/" : "") + storageName;
+            return baseFolder + folderType.name().toLowerCase() + "/" + relatedNumber + "/" + (order > 0 ? order + "/" : "") + storageName;
         }
 
-        //유효하지 않은 relatedType일 경우 예외처리
+        // 유효하지 않은 relatedType일 경우 예외처리
         else {
             throw new IllegalArgumentException("커뮤니티 게시물이 유효하지 않는 타입입니다.: " + relatedType);
         }
     }
 
-    //임시저장 key 생성 메소드 {baseFolder}/{relatedType}/{temparary}/{file_name}
+
+    // 임시저장 key 생성 메소드: {baseFolder}/{relatedType}/{temporary}/{file_name}
     public String generateTempS3Key(String relatedType, String storageName) {
 
         FileFolder folderType = FileFolder.from(relatedType);
 
-        //profile 인 경우
+        // profile인 경우
         if (folderType == FileFolder.PROFILE) {
             return baseFolder + folderType.name().toLowerCase() + "/" + "temporary" + "/" + storageName;
         }
 
-        //커뮤니티 인경우
+        // 커뮤니티인 경우
         else if (folderType == FileFolder.COMMUNITY) {
             return baseFolder + folderType.name().toLowerCase() + "/" + "temporary" + "/" + storageName;
         }
 
-        //유효하지 않은 relatedType일 경우 예외처리
+        // 유효하지 않은 relatedType일 경우 예외처리
         else {
             throw new IllegalArgumentException("커뮤니티 게시물이 유효하지 않는 타입입니다.: " + relatedType);
         }
     }
-
 
 
     //path 를 찾는 메소드
@@ -68,17 +75,17 @@ public class S3KeyHandler {
 
         FileFolder folderType = FileFolder.from(relatedType);
 
-        //profile 인 경우
+        // profile인 경우
         if (folderType == FileFolder.PROFILE) {
             return baseFolder + folderType.name().toLowerCase() + "/" + relatedNumber + "/";
         }
 
-        //커뮤니티 인경우
+        // 커뮤니티인 경우
         else if (folderType == FileFolder.COMMUNITY) {
             return baseFolder + folderType.name().toLowerCase() + "/" + relatedNumber + "/" + (order > 0 ? order + "/" : "");
         }
 
-        //유효하지 않은 relatedType일 경우 예외처리
+        // 유효하지 않은 relatedType일 경우 예외처리
         else {
             throw new IllegalArgumentException("커뮤니티 게시물이 유효하지 않는 타입입니다.: " + relatedType);
         }
@@ -87,19 +94,21 @@ public class S3KeyHandler {
 
     //url로 key를 추출하는 메소드
     public String getKeyByUrl(String url) {
-        System.out.println("S3 key로 url 추출 메소드 동작, url : " + url);
-        String bucketName = s3Component.getBucket();
-        String region = "ap-northeast-2";
-        String S3_URL_PREFIX = "https://"+ bucketName +".s3." + region + ".amazonaws.com/";
-        System.out.println("S3 URL Prefix: " + S3_URL_PREFIX);
+        log.info("S3KeyHandler - Url로 S3 key 추출 url:{}", url);
+
+        //String bucketName = s3Component.getBucket();
+        //String region = "ap-northeast-2";
+
+        String S3_URL_PREFIX = "https://" + bucketName + ".s3." + S3_REGION + ".amazonaws.com/";
+        log.info("S3 URL Prefix:{}", S3_URL_PREFIX);
 
         // URL이 올바른 형식인지 확인
         if (!url.startsWith(S3_URL_PREFIX)) {
-            throw new IllegalArgumentException("URL 형식이 올바르지 않습니다. S3 URL인지 확인해주세요");
+            log.warn("S3KeyHandler - 옳지 않은 형식의 URL입니다. url:{}", url);
+            throw new IllegalArgumentException("URL 형식이 올바르지 않습니다. S3 URL인지 확인해주세요.");
         }
-        String key = url.replace(S3_URL_PREFIX, "");
 
-
-        return key;
+        return url.replace(S3_URL_PREFIX, "");
     }
+
 }
