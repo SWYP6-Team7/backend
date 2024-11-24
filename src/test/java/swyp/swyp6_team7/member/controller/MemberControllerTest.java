3f90a78f455c56c56c95a1f23f4e3d292af0c468
Yujeong <22mallow3@gmail.com>
2024-11-24 14:54:19 +0900

    chore(test): 불필요한 코드 제거

--- src/test/java/swyp/swyp6_team7/member/controller/MemberControllerTest.java
+++ src/test/java/swyp/swyp6_team7/member/controller/MemberControllerTest.java
@@ -29,14 +29,6 @@ import static org.mockito.ArgumentMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
-@TestPropertySource(properties = {
-        "kakao.client-id=fake-client-id",
-        "kakao.client-secret=fake-client-secret",
-        "kakao.redirect-uri=http://localhost:8080/login/oauth2/code/kakao",
-        "kakao.token-url=https://kauth.kakao.com/oauth/token",
-        "kakao.user-info-url=https://kapi.kakao.com/v2/user/me"
-})
 public class MemberControllerTest {
 
     @Autowired
