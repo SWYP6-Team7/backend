2a2e78abdeef633eb64f1cfc2dee93663aee2034
sujnkim <blackout3210@naver.com>
2024-12-02 21:43:10 +0900

    test(image): 커뮤니티 이미지 정식 저장 기능 테스트 작성

--- src/test/java/swyp/swyp6_team7/image/controller/ImageCommunityControllerTest.java
+++ src/test/java/swyp/swyp6_team7/image/controller/ImageCommunityControllerTest.java
@@ -3,7 +3,6 @@ package swyp.swyp6_team7.image.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,17 +11,20 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import swyp.swyp6_team7.image.dto.request.CommunityImageSaveRequest;
 import swyp.swyp6_team7.image.dto.response.ImageDetailResponseDto;
 import swyp.swyp6_team7.image.service.ImageCommunityService;
-import swyp.swyp6_team7.image.service.ImageProfileService;
-import swyp.swyp6_team7.image.service.ImageService;
 import swyp.swyp6_team7.mock.WithMockCustomUser;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -77,4 +79,61 @@ class ImageCommunityControllerTest {
                 .andExpect(jsonPath("$.uploadDate").value("2024년 11월 24일 10시 00분"));
     }
 
+    @DisplayName("saveImages: 임시 저장 커뮤니티 이미지를 정식으로 저장한다.")
+    @WithMockCustomUser
+    @Test
+    void saveImages() throws Exception {
+        // given
+        int postNumber = 2;
+        List<String> deletedUrls = List.of("https://bucketName.s3.region.amazonaws.com/baseFolder/community/temporary/storageName.png");
+        List<String> tempUrls = List.of(
+                "https://bucketName.s3.region.amazonaws.com/baseFolder/community/temporary/storageName2.png",
+                "https://bucketName.s3.region.amazonaws.com/baseFolder/community/temporary/storageName3.png");
+        CommunityImageSaveRequest request = new CommunityImageSaveRequest(deletedUrls, tempUrls);
+
+        ImageDetailResponseDto image1 = createdDetailResponse(1, postNumber,
+                "baseFolder/community/temporary/storageName2.png",
+                "https://bucketName.s3.region.amazonaws.com/baseFolder/community/temporary/storageName2.png");
+        ImageDetailResponseDto image2 = createdDetailResponse(2, postNumber,
+                "baseFolder/community/temporary/storageName3.png",
+                "https://bucketName.s3.region.amazonaws.com/baseFolder/community/temporary/storageName3.png");
+        List<ImageDetailResponseDto> response = List.of(image1, image2);
+
+        given(imageCommunityService.saveCommunityImages(anyInt(), anyList(), anyList()))
+                .willReturn(response);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(post("/api/community/{postNumber}/images", postNumber)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(request)));
+
+        // then
+        resultActions.andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$.[0].imageNumber").value(1L))
+                .andExpect(jsonPath("$.[0].relatedType").value("community"))
+                .andExpect(jsonPath("$.[0].relatedNumber").value(2))
+                .andExpect(jsonPath("$.[0].key").value("baseFolder/community/temporary/storageName2.png"))
+                .andExpect(jsonPath("$.[0].url").value("https://bucketName.s3.region.amazonaws.com/baseFolder/community/temporary/storageName2.png"))
+                .andExpect(jsonPath("$.[0].uploadDate").value("2024년 11월 24일 10시 00분"))
+                .andExpect(jsonPath("$.[1].imageNumber").value(2L))
+                .andExpect(jsonPath("$.[1].relatedType").value("community"))
+                .andExpect(jsonPath("$.[1].relatedNumber").value(2))
+                .andExpect(jsonPath("$.[1].key").value("baseFolder/community/temporary/storageName3.png"))
+                .andExpect(jsonPath("$.[1].url").value("https://bucketName.s3.region.amazonaws.com/baseFolder/community/temporary/storageName3.png"))
+                .andExpect(jsonPath("$.[1].uploadDate").value("2024년 11월 24일 10시 00분"));
+        then(imageCommunityService).should(times(1))
+                .saveCommunityImages(eq(postNumber), eq(deletedUrls), eq(tempUrls));
+    }
+
+    private ImageDetailResponseDto createdDetailResponse(long imageNumber, int relatedNumber, String key, String url) {
+        return ImageDetailResponseDto.builder()
+                .imageNumber(imageNumber)
+                .relatedType("community")
+                .relatedNumber(relatedNumber)
+                .key(key)
+                .url("https://bucketName.s3.region.amazonaws.com/" + key)
+                .uploadDate(LocalDateTime.of(2024, 11, 24, 10, 0, 0))
+                .build();
+    }
 }
--- src/test/java/swyp/swyp6_team7/image/service/ImageCommunityServiceTest.java
+++ src/test/java/swyp/swyp6_team7/image/service/ImageCommunityServiceTest.java
@@ -3,24 +3,26 @@ package swyp.swyp6_team7.image.service;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import swyp.swyp6_team7.image.domain.Image;
 import swyp.swyp6_team7.image.dto.response.ImageDetailResponseDto;
 import swyp.swyp6_team7.image.repository.ImageRepository;
 import swyp.swyp6_team7.image.s3.S3Uploader;
 import swyp.swyp6_team7.image.util.S3KeyHandler;
-import swyp.swyp6_team7.image.util.StorageNameHandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 
 @SpringBootTest
 class ImageCommunityServiceTest {
@@ -73,4 +75,70 @@ class ImageCommunityServiceTest {
                 .contains("community", 0, "baseFolder/community/temporary/storageName.png", "https://bucketName.s3.region.amazonaws.com/baseFolder/community/temporary/storageName.png");
     }
 
+    @DisplayName("saveCommunityImages: 커뮤니티 글에 대한 이미지 정식 저장")
+    @Test
+    void saveCommunityImages() {
+        // given
+        int postNumber = 2;
+        Image image1 = imageRepository.save(createTempImage("image1"));
+        Image image2 = imageRepository.save(createTempImage("image2"));
+        Image image3 = imageRepository.save(createTempImage("image3"));
+
+        List<String> deletedUrls = List.of(image1.getUrl());
+        List<String> tempUrls = List.of(image2.getUrl(), image3.getUrl());
+
+        given(s3Uploader.existObject(anyString())).willReturn(true);
+        doNothing().when(s3Uploader).deleteFile(anyString());
+        given(s3Uploader.moveImage(anyString(), anyString()))
+                .willReturn("");
+
+        given(s3KeyHandler.getKeyByUrl("https://bucketName.s3.ap-northeast-2.amazonaws.com/baseFolder/community/temporary/stored-image2"))
+                .willReturn("baseFolder/community/temporary/stored-image2");
+        given(s3KeyHandler.getKeyByUrl("https://bucketName.s3.ap-northeast-2.amazonaws.com/baseFolder/community/temporary/stored-image3"))
+                .willReturn("baseFolder/community/temporary/stored-image3");
+        given(s3KeyHandler.generateS3Key("community", 2, "stored-image2", 1))
+                .willReturn("baseFolder/community/2/1/stored-image2.png");
+        given(s3KeyHandler.generateS3Key("community", 2, "stored-image3", 2))
+                .willReturn("baseFolder/community/2/2/stored-image3.png");
+
+        given(s3Uploader.getImageUrl("baseFolder/community/2/1/stored-image2.png"))
+                .willReturn("https://bucketName.s3.ap-northeast-2.amazonaws.com/baseFolder/community/2/1/stored-image2.png");
+        given(s3Uploader.getImageUrl("baseFolder/community/2/2/stored-image3.png"))
+                .willReturn("https://bucketName.s3.ap-northeast-2.amazonaws.com/baseFolder/community/2/2/stored-image3.png");
+
+        // when
+        List<ImageDetailResponseDto> savedImages = imageCommunityService.saveCommunityImages(postNumber, deletedUrls, tempUrls);
+
+        // then
+        assertThat(imageRepository.findAll()).hasSize(2)
+                .extracting("imageNumber", "relatedType", "relatedNumber", "order", "key", "url", "originalName", "storageName", "size", "format")
+                .containsExactlyInAnyOrder(
+                        tuple(image2.getImageNumber(), "community", postNumber, 1, "baseFolder/community/2/1/stored-image2.png", "https://bucketName.s3.ap-northeast-2.amazonaws.com/baseFolder/community/2/1/stored-image2.png", "image2", "stored-image2", 2266L, "image/png"),
+                        tuple(image3.getImageNumber(), "community", postNumber, 2, "baseFolder/community/2/2/stored-image3.png", "https://bucketName.s3.ap-northeast-2.amazonaws.com/baseFolder/community/2/2/stored-image3.png", "image3", "stored-image3", 2266L, "image/png")
+                );
+
+        assertThat(savedImages)
+                .extracting("relatedType", "relatedNumber", "key", "url")
+                .containsExactlyInAnyOrder(
+                        tuple("community", 2, "baseFolder/community/2/1/stored-image2.png", "https://bucketName.s3.ap-northeast-2.amazonaws.com/baseFolder/community/2/1/stored-image2.png"),
+                        tuple("community", 2, "baseFolder/community/2/2/stored-image3.png", "https://bucketName.s3.ap-northeast-2.amazonaws.com/baseFolder/community/2/2/stored-image3.png")
+                );
+    }
+
+    private Image createTempImage(String imageName) {
+        String storageName = "stored-" + imageName;
+        String key = "baseFolder/community/temporary/" + storageName;
+        return Image.builder()
+                .originalName(imageName)
+                .storageName(storageName)
+                .size(2266L)
+                .format("image/png")
+                .relatedType("community")
+                .relatedNumber(0)
+                .order(0)
+                .key(key)
+                .url("https://bucketName.s3.ap-northeast-2.amazonaws.com/" + key)
+                .uploadDate(LocalDateTime.of(2024, 11, 29, 12, 0))
+                .build();
+    }
 }
