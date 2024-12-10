83838f6ff1a43aae42f17f25da76a69ccbd2ac7f
Yujeong <22mallow3@gmail.com>
2024-12-11 02:29:34 +0900

    refactor: userNumber가 null일 경우의 좋아요 상태 로직 수정

--- src/main/java/swyp/swyp6_team7/likes/util/LikeStatus.java
+++ src/main/java/swyp/swyp6_team7/likes/util/LikeStatus.java
@@ -7,13 +7,14 @@ public class LikeStatus {
 
 
     // 댓글 조회 시 필요한 좋아요 상태
-    public static LikeReadResponseDto getLikeStatus(LikeRepository likeRepository, String relatedType, int relatedNumber, int userNumber) {
+    public static LikeReadResponseDto getLikeStatus(LikeRepository likeRepository, String relatedType, int relatedNumber, Integer  userNumber) {
+        if (userNumber == null) {
+            return new LikeReadResponseDto(relatedType,relatedNumber,false, likeRepository.countByRelatedTypeAndRelatedNumber(relatedType, relatedNumber));
+        }
         // 좋아요 여부 확인
         boolean liked = likeRepository.existsByRelatedTypeAndRelatedNumberAndUserNumber(relatedType, relatedNumber, userNumber);
-
         // 총 좋아요 수 가져오기
         long likes = likeRepository.countByRelatedTypeAndRelatedNumber(relatedType, relatedNumber);
-
         // DTO 생성 및 반환
         return new LikeReadResponseDto(relatedType, relatedNumber, liked, likes);
     }
