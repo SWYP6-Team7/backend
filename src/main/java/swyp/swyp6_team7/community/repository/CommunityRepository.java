66596ec5abf7fa8c383f015daac79c087d204a7b
Yujeong <22mallow3@gmail.com>
2024-12-19 23:22:46 +0900

    fix: findByPostNumber 메서드에 JPQL 추가로 잘못된 쿼리 동작 수정

--- src/main/java/swyp/swyp6_team7/community/repository/CommunityRepository.java
+++ src/main/java/swyp/swyp6_team7/community/repository/CommunityRepository.java
@@ -1,6 +1,8 @@
 package swyp.swyp6_team7.community.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import swyp.swyp6_team7.community.domain.Community;
 
@@ -10,7 +12,8 @@ import java.util.Optional;
 public interface CommunityRepository extends JpaRepository<Community, Integer> {
 
     //게시물 상세 조회
-    Optional<Community> findByPostNumber(int postNumber);
+    @Query("SELECT c FROM Community c WHERE c.postNumber = :postNumber")
+    Optional<Community> findByPostNumber(@Param("postNumber") int postNumber);
     boolean existsByPostNumber(Integer postNumber);
 
 }
