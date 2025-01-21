7a5fad8c7ac96df4b6f3ef928f6c5aafe7859064
doyoubkwon <kdy162424@gmail.com>
2025-01-22 01:02:18 +0900

    [TMO-39] 이메일 기반 회원가입 API 인증 해제

--- src/main/java/swyp/swyp6_team7/global/config/WebMvcConfig.java
+++ src/main/java/swyp/swyp6_team7/global/config/WebMvcConfig.java
@@ -1,10 +1,6 @@
 package swyp.swyp6_team7.global.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
