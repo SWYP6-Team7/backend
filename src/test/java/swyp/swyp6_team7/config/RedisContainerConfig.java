b39db8be0ce509935542a13cade79204a655bd9c
sujnkim <blackout3210@naver.com>
2025-02-04 01:07:40 +0900

    chore(redis): testContainer 의존성 추가 및 RedisConfig 수정

---
+++ src/test/java/swyp/swyp6_team7/config/RedisContainerConfig.java
@@ -0,0 +1,25 @@
+package swyp.swyp6_team7.config;
+
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
+
+public class RedisContainerConfig implements BeforeAllCallback {
+
+    private static final String REDIS_DOCKER_IMAGE = "redis:6.2-alpine";
+    private static final int REDIS_PORT = 6379;
+    private static GenericContainer redisContainer;
+
+    @Override
+    public void beforeAll(ExtensionContext context) throws Exception {
+        redisContainer = new GenericContainer<>(DockerImageName.parse(REDIS_DOCKER_IMAGE))
+                .withExposedPorts(REDIS_PORT);
+
+        redisContainer.start();
+
+        System.setProperty("redis.host", redisContainer.getHost());
+        System.setProperty("redis.port", redisContainer.getMappedPort(REDIS_PORT).toString());
+    }
+}
