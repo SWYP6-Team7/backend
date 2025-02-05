044cc2618052e0bdfa75d7aee993f09720563322
sujnkim <blackout3210@naver.com>
2025-02-05 22:47:48 +0900

    fix(test): testcontainer 설정 수정

--- src/test/java/swyp/swyp6_team7/config/RedisContainerConfig.java
+++ src/test/java/swyp/swyp6_team7/config/RedisContainerConfig.java
@@ -3,6 +3,7 @@ package swyp.swyp6_team7.config;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 
 
@@ -15,7 +16,8 @@ public class RedisContainerConfig implements BeforeAllCallback {
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
         redisContainer = new GenericContainer<>(DockerImageName.parse(REDIS_DOCKER_IMAGE))
-                .withExposedPorts(REDIS_PORT);
+                .withExposedPorts(REDIS_PORT)
+                .waitingFor(Wait.forListeningPort());
 
         redisContainer.start();
 
