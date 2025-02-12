2b6e98f2f978cc545d82897d36af6397fecdf558
doyoubkwon <kdy162424@gmail.com>
2025-02-13 04:48:58 +0900

    [TMO-50] LettuceConnection rollback

--- src/test/java/swyp/swyp6_team7/config/RedisContainerConfig.java
+++ src/test/java/swyp/swyp6_team7/config/RedisContainerConfig.java
@@ -1,6 +1,10 @@
 package swyp.swyp6_team7.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -16,5 +20,16 @@ public class RedisContainerConfig {
         container = new GenericContainer<>("redis:latest")
                 .withExposedPorts(REDIS_PORT);
         container.start();
+    }
+
+    @Primary
+    @Bean
+    public LettuceConnectionFactory testRedisConnectionFactory() {
+        return new LettuceConnectionFactory(
+                new RedisStandaloneConfiguration(
+                        container.getHost(),
+                        container.getMappedPort(REDIS_PORT)
+                )
+        );
     }
 }
