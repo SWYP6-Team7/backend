7aa0eddf5ec6fe110fb26f3a2b5501babe2ee5ca
Yujeong <22mallow3@gmail.com>
2025-03-06 23:09:13 +0900

    refactor: 애플리케이션 실행 시 location 삽입 로직 제거

--- src/main/java/swyp/swyp6_team7/location/controller/LocationController.java
+++ src/main/java/swyp/swyp6_team7/location/controller/LocationController.java
@@ -1,5 +1,6 @@
 package swyp.swyp6_team7.location.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,13 +13,10 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/autocomplete")
 public class LocationController {
     private final LocationAutocompleteService locationAutocompleteService;
-
-    public LocationController(LocationAutocompleteService locationAutocompleteService) {
-        this.locationAutocompleteService = locationAutocompleteService;
-    }
 
     @GetMapping
     public ApiResponse<AutoCompleteSuggestion> getAutocompleteSuggestions(@RequestParam(value = "location", required = false) String location) {
