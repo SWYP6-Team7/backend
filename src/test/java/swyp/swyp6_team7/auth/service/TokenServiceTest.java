e9fdad2f6d716bea9181bfb76f7bbe75af495a60
doyoubkwon <kdy162424@gmail.com>
2025-03-20 13:11:57 +0900

    [TMO-64] LoginToken 응답 수정

--- src/test/java/swyp/swyp6_team7/auth/service/TokenServiceTest.java
+++ src/test/java/swyp/swyp6_team7/auth/service/TokenServiceTest.java
@@ -8,18 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
+import swyp.swyp6_team7.auth.dto.LoginTokenResponse;
 import swyp.swyp6_team7.auth.jwt.JwtProvider;
 import swyp.swyp6_team7.member.entity.Users;
 import swyp.swyp6_team7.member.repository.UserRepository;
 
 import java.time.Duration;
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 class TokenServiceTest {
@@ -31,10 +31,10 @@ class TokenServiceTest {
     private RedisTemplate<String, String> redisTemplate;
     @MockBean
     private UserRepository userRepository;
-    private ValueOperations<String,String> valueOperations;
+    private ValueOperations<String, String> valueOperations;
 
     @BeforeEach
-    void setup(){
+    void setup() {
         jwtProvider = mock(JwtProvider.class);
         redisTemplate = mock(RedisTemplate.class);
         userRepository = mock(UserRepository.class);
@@ -45,19 +45,19 @@ class TokenServiceTest {
 
     @Test
     @DisplayName("유효한 RefreshToken으로 새로운 AccessToken 생성")
-    void refreshWithLock_success(){
+    void refreshWithLock_success() {
         String refreshToken = "validRefreshToken";
         Integer userNumber = 12345;
-        String lockKey = "refreshTokenLock:"+refreshToken;
-        String accessCacheKey = "refreshTokenCache:"+refreshToken;
+        String lockKey = "refreshTokenLock:" + refreshToken;
+        String accessCacheKey = "refreshTokenCache:" + refreshToken;
         String newAccessToken = "newAccessToken";
         String newRefreshToken = "newRefreshToken";
         long ttl = 900;
 
-        when(valueOperations.setIfAbsent(eq(lockKey), eq("LOCKED"),eq(Duration.ofSeconds(5)))).thenReturn(true);
+        when(valueOperations.setIfAbsent(eq(lockKey), eq("LOCKED"), eq(Duration.ofSeconds(5)))).thenReturn(true);
         when(jwtProvider.validateToken(refreshToken)).thenReturn(true);
         when(jwtProvider.getUserNumber(refreshToken)).thenReturn(userNumber);
-        when(valueOperations.get("refreshToken:"+userNumber)).thenReturn(refreshToken);
+        when(valueOperations.get("refreshToken:" + userNumber)).thenReturn(refreshToken);
         Users user = new Users();
         when(userRepository.findByUserNumber(userNumber)).thenReturn(Optional.of(user));
         when(jwtProvider.createAccessToken(eq(userNumber), anyList())).thenReturn("newAccessToken");
@@ -67,22 +67,22 @@ class TokenServiceTest {
         // 캐시된 Access Token이 없는 상태를 시뮬레이션
         when(valueOperations.get(eq(accessCacheKey))).thenReturn(null);
 
-        Map<String, String> result = tokenService.refreshWithLock(refreshToken);
+        LoginTokenResponse result = tokenService.refreshWithLock(refreshToken);
 
         // 새로운 Access Token이 Redis에 캐시되었는지 검증
         verify(valueOperations).set(eq(accessCacheKey), eq(newAccessToken), eq(Duration.ofSeconds(ttl)));
-        assertThat(result.get("accessToken")).isEqualTo(newAccessToken);
-        assertThat(result.get("refreshToken")).isEqualTo(newRefreshToken);
+        assertThat(result.getAccessToken()).isEqualTo(newAccessToken);
+        assertThat(result.getRefreshToken()).isEqualTo(newRefreshToken);
         verify(redisTemplate).delete(lockKey);
     }
 
     @Test
     @DisplayName("유효하지 않은 RefreshToken 처리 - JwtException")
-    void refreshWithLock_invalidToken(){
+    void refreshWithLock_invalidToken() {
         String refreshToken = "InvalidRefreshToken";
-        String lockKey = "refreshTokenLock:"+refreshToken;
+        String lockKey = "refreshTokenLock:" + refreshToken;
 
-        when(valueOperations.setIfAbsent(eq(lockKey),eq("LOCKED"), eq(Duration.ofSeconds(5)))).thenReturn(true);
+        when(valueOperations.setIfAbsent(eq(lockKey), eq("LOCKED"), eq(Duration.ofSeconds(5)))).thenReturn(true);
         when(jwtProvider.validateToken(refreshToken)).thenReturn(false);
 
         JwtException exception = assertThrows(JwtException.class, () -> tokenService.refreshWithLock(refreshToken));
@@ -94,9 +94,9 @@ class TokenServiceTest {
     void refreshWithLock_userNotFound() {
         String refreshToken = "validRefreshToken";
         Integer userNumber = 12345;
-        String lockKey = "refreshTokenLock:"+refreshToken;
+        String lockKey = "refreshTokenLock:" + refreshToken;
 
-        when(valueOperations.setIfAbsent(eq(lockKey),eq("LOCKED"),eq(Duration.ofSeconds(5)))).thenReturn(true);
+        when(valueOperations.setIfAbsent(eq(lockKey), eq("LOCKED"), eq(Duration.ofSeconds(5)))).thenReturn(true);
         when(jwtProvider.validateToken(refreshToken)).thenReturn(true);
         when(jwtProvider.getUserNumber(refreshToken)).thenReturn(userNumber);
         when(userRepository.findByUserNumber(userNumber)).thenReturn(Optional.empty());
@@ -107,11 +107,11 @@ class TokenServiceTest {
 
     @Test
     @DisplayName("동시 요청 시 Lock 획득 실패")
-    void refreshWithLock_concurrentRequest(){
+    void refreshWithLock_concurrentRequest() {
         String refreshToken = "validRefreshToken";
-        String lockKey = "refreshTokenLock:"+refreshToken;
+        String lockKey = "refreshTokenLock:" + refreshToken;
 
-        when(valueOperations.setIfAbsent(eq(lockKey),eq("LOCKED"),eq(Duration.ofSeconds(5)))).thenReturn(false);
+        when(valueOperations.setIfAbsent(eq(lockKey), eq("LOCKED"), eq(Duration.ofSeconds(5)))).thenReturn(false);
 
         JwtException exception = assertThrows(JwtException.class, () -> tokenService.refreshWithLock(refreshToken));
         assertEquals("유효하지 않은 RefreshToken입니다.", exception.getMessage());
@@ -119,16 +119,17 @@ class TokenServiceTest {
 
     @Test
     @DisplayName("예상치 못한 오류 - 일반 예외")
-    void refreshWithLock_unexpectedException(){
+    void refreshWithLock_unexpectedException() {
         String refreshToken = "validRefreshToken";
-        String lockKey = "refreshTokenLock:"+refreshToken;
+        String lockKey = "refreshTokenLock:" + refreshToken;
 
-        when(valueOperations.setIfAbsent(eq(lockKey), eq("LOCKED"),eq(Duration.ofSeconds(5)))).thenReturn(true);
+        when(valueOperations.setIfAbsent(eq(lockKey), eq("LOCKED"), eq(Duration.ofSeconds(5)))).thenReturn(true);
         when(jwtProvider.validateToken(refreshToken)).thenThrow(new RuntimeException("Unexpected Error"));
 
-        RuntimeException exception = assertThrows(RuntimeException.class,()->tokenService.refreshWithLock(refreshToken));
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> tokenService.refreshWithLock(refreshToken));
         assertEquals("Unexpected Error", exception.getMessage());
     }
+
     @Test
     @DisplayName("유효한 RefreshToken으로 Redis 캐싱과 TTL 설정 확인")
     void testRefreshTokenCachingAndTTL() {
@@ -138,14 +139,14 @@ class TokenServiceTest {
         Integer userNumber = 12345;
         long ttl = 900; // 15분
         String lockKey = "refreshTokenLock:" + refreshToken;
-        String accessCacheKey = "refreshTokenCache:"+refreshToken;
+        String accessCacheKey = "refreshTokenCache:" + refreshToken;
 
         when(valueOperations.setIfAbsent(eq(lockKey), eq("LOCKED"), eq(Duration.ofSeconds(5))))
                 .thenReturn(true);
 
         when(jwtProvider.validateToken(refreshToken)).thenReturn(true);
         when(jwtProvider.getUserNumber(refreshToken)).thenReturn(userNumber);
-        when(valueOperations.get("refreshToken:"+userNumber)).thenReturn(refreshToken);
+        when(valueOperations.get("refreshToken:" + userNumber)).thenReturn(refreshToken);
         Users user = new Users();
         when(userRepository.findByUserNumber(userNumber)).thenReturn(Optional.of(user));
         when(jwtProvider.createAccessToken(userNumber, List.of("ROLE_USER"))).thenReturn(newAccessToken);
@@ -153,15 +154,15 @@ class TokenServiceTest {
         when(jwtProvider.getRemainingValidity(newAccessToken)).thenReturn(ttl);
 
         when(valueOperations.get(eq(accessCacheKey))).thenReturn(null);
-        Map<String, String> result = tokenService.refreshWithLock(refreshToken);
+        LoginTokenResponse result = tokenService.refreshWithLock(refreshToken);
 
         verify(valueOperations).set(
                 eq("refreshTokenCache:" + refreshToken),
                 eq(newAccessToken),
                 eq(Duration.ofSeconds(ttl))
         );
-        assertThat(result.get("accessToken")).isEqualTo(newAccessToken);
-        assertThat(result.get("refreshToken")).isEqualTo(newRefreshToken);
+        assertThat(result.getAccessToken()).isEqualTo(newAccessToken);
+        assertThat(result.getRefreshToken()).isEqualTo(newRefreshToken);
         verify(redisTemplate).delete(lockKey);
     }
 
@@ -176,11 +177,11 @@ class TokenServiceTest {
         String lockKey = "refreshTokenLock:" + refreshToken;
         String accessCacheKey = "refreshTokenCache:" + refreshToken;
 
-        when(valueOperations.setIfAbsent(eq(lockKey),eq("LOCKED"),eq(Duration.ofSeconds(5))))
+        when(valueOperations.setIfAbsent(eq(lockKey), eq("LOCKED"), eq(Duration.ofSeconds(5))))
                 .thenReturn(true);
         when(jwtProvider.validateToken(refreshToken)).thenReturn(true);
         when(jwtProvider.getUserNumber(refreshToken)).thenReturn(userNumber);
-        when(valueOperations.get("refreshToken:"+userNumber)).thenReturn(refreshToken);
+        when(valueOperations.get("refreshToken:" + userNumber)).thenReturn(refreshToken);
         Users user = new Users();
         when(userRepository.findByUserNumber(userNumber)).thenReturn(Optional.of(user));
         when(jwtProvider.createAccessToken(userNumber, List.of("ROLE_USER"))).thenReturn(newAccessToken);
@@ -190,8 +191,8 @@ class TokenServiceTest {
         // 처음 캐시에는 값이 존재함
         when(valueOperations.get(eq(accessCacheKey))).thenReturn(newAccessToken);
 
-        Map<String, String> result = tokenService.refreshWithLock(refreshToken);
-        assertThat(result.get("accessToken")).isEqualTo(newAccessToken);
+        LoginTokenResponse result = tokenService.refreshWithLock(refreshToken);
+        assertThat(result.getAccessToken()).isEqualTo(newAccessToken);
 
         // stubbing 충돌을 방지하기 위해 valueOperations 모의를 재설정하고 다시 stubbing
         reset(valueOperations);
