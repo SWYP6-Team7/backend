8da2e6f1103b5c3dfb9ad0aea1a415b5f5cb8263
doyoubkwon <kdy162424@gmail.com>
2025-03-21 16:17:27 +0900

    [TMO-65] Redis container 롤백

--- src/test/java/swyp/swyp6_team7/config/RedisContainerConfig.java
+++ src/test/java/swyp/swyp6_team7/config/RedisContainerConfig.java
@@ -7,30 +7,28 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration
 @Testcontainers
 public class RedisContainerConfig {
 
     private static final int REDIS_PORT = 6379;
-    
-    @Bean
-    public GenericContainer<?> redisContainer() {
-        GenericContainer<?> container = new GenericContainer<>(DockerImageName.parse("redis:latest"))
+
+    private static final GenericContainer<?> container;
+
+    static {
+        container = new GenericContainer<>("redis:latest")
                 .withExposedPorts(REDIS_PORT);
         container.start();
-        System.out.println("Redis TestContainer started at: " + container.getHost() + ":" + container.getMappedPort(REDIS_PORT));
-        return container;
     }
 
     @Primary
     @Bean
-    public LettuceConnectionFactory testRedisConnectionFactory(GenericContainer<?> redisContainer) {
+    public LettuceConnectionFactory testRedisConnectionFactory() {
         return new LettuceConnectionFactory(
                 new RedisStandaloneConfiguration(
-                        redisContainer.getHost(),
-                        redisContainer.getMappedPort(REDIS_PORT)
+                        container.getHost(),
+                        container.getMappedPort(REDIS_PORT)
                 )
         );
     }
