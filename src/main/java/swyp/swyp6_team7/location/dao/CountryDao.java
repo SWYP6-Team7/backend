4e33595fa10c884679bd6750b4cd34966112fe7c
Yujeong <22mallow3@gmail.com>
2025-04-15 13:23:15 +0900

    [TMO-62] locations 테이블 데이터 적재 로직수정

--- src/main/java/swyp/swyp6_team7/location/dao/CountryDao.java
+++ src/main/java/swyp/swyp6_team7/location/dao/CountryDao.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 import swyp.swyp6_team7.location.domain.Country;
 import swyp.swyp6_team7.location.domain.Continent;
 
+import java.util.Optional;
+
 @Repository
 public class CountryDao {
     private final JdbcTemplate jdbcTemplate;
@@ -37,4 +39,29 @@ public class CountryDao {
         Integer count = jdbcTemplate.queryForObject(sql, new Object[]{countryName}, Integer.class);
         return count != null && count > 0;
     }
+
+    public Optional<Country> findByCountryName(String countryName) {
+        String sql = "SELECT * FROM countries WHERE country_name = ?";
+        return jdbcTemplate.query(sql, new Object[]{countryName}, rs -> {
+            if (rs.next()) {
+                Country country = new Country();
+                country.setId(rs.getInt("country_id"));
+                country.setCountryName(rs.getString("country_name"));
+                country.setLatitude(rs.getDouble("latitude"));
+                country.setLongitude(rs.getDouble("longitude"));
+                country.setContinent(Continent.valueOf(rs.getString("continent")));
+                return Optional.of(country);
+            }
+            return Optional.empty();
+        });
+    }
+
+    public Country insertCountry(String countryName, Continent continent) {
+        String sql = "INSERT INTO countries (country_name, continent) VALUES (?, ?)";
+        jdbcTemplate.update(sql, countryName, continent.name());
+
+        return findByCountryName(countryName).orElseThrow(() ->
+                new IllegalStateException("Country insert failed: " + countryName));
+    }
+
 }
